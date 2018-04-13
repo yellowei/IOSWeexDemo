ad9cc0ed31a1acc91959790066ea244f91da274c
Swift <swift@swiftdemacbook-air.local>
2018-04-14 00:24:12 +0800

    Initial Commit

---
+++ WeexDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WeexDemo
+//
+//  Created by Swift on 2018/4/14.
+//  Copyright © 2018年 Swift. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ WeexDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WeexDemo
+//
+//  Created by Swift on 2018/4/14.
+//  Copyright © 2018年 Swift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
